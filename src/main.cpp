7c9c7a95a29c5716d38203762337fbd900b47568
Azkie <aliens.oh.3304@gmail.com>
2026-06-11 21:48:47 +0900

    Environment construction

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+
+/* myjson ****************************************/
+     constexpr std::string_view VERSION = "0.0";
+/*                                               */
+/*   made by Azkey                               */
+/*************************************************/
+
+int main( int argc, char* argv[] ){
+    std::cout << "Hello, myjson\n";
+    return 0;
+}
